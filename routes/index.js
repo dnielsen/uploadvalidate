a9ed6433c91249ea9f019c62d5740cc762a8e234
ashish kumar <ashish@xodotech.com>
2018-07-07 05:02:58 +0530

    UPDATE: 1. Page one done 2. Image chaining done (doublly linked list)

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,111 @@
-var express = require('express');
+const express = require('express');
+const redis = require('redis');
+
+const multer = require('multer');
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, 'public/images/uploads')
+    },
+    filename: (req, file, cb) => {
+      cb(null, file.originalname);
+    }
+});
+const upload = multer({storage: storage});
+
 var router = express.Router();
+
+const redisClient = redis.createClient(); //default port : 6379
+
+//previous uploaded image
+let lastImage = null;
+let lastVerifiedImage = null;
+
+//redis connection
+redisClient.on('connect', () => {
+  console.log('Redis client connected');
+  redisClient.get('lastImage', (error, imageName) => {
+    if(error)
+      throw error;
+    
+    if(imageName != null)
+      lastImage = imageName; // last uploaded image
+  });
+  redisClient.get('lastVerifiedImage', (error, imageName) => {
+    if(error)
+      throw error;
+    
+    if(imageName != null)
+      lastVerifiedImage = imageName; // last verified image
+  });
+});
+
+redisClient.on('error', error => console.log('Redis client error: ' + error));
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Upload Validate' });
+  res.render('index', { title: 'Upload Validate', categories: [1,2,3,4,5,6,7,8,9,10] });
+});
+
+router.post('/',upload.single('image'), function(req, res, next) {
+  
+  redisClient.hset(`image:${req.file.originalname}`, 'categoryID', `${req.body.category}`, redis.print);
+  redisClient.hdel(`image:${req.file.originalname}`, 'rating', redis.print); // delete rating
+  if(lastVerifiedImage === null){
+    lastVerifiedImage = req.file.originalname; //update last verified image
+    redisClient.set('lastVerifiedImage', lastVerifiedImage, redis.print); //persist
+  }
+  redisClient.hmget(`image:${req.file.originalname}`, 'previous', 'next', (error, reference) => {
+    if(error)
+      throw error;
+      
+    let nodeType = 'new';
+    let lastImageCopy = lastImage;
+    
+    if(reference[0] === null && reference[1] !== null)
+      nodeType = 'first';
+    else if(reference[0] !== null && reference[1] === null)
+      nodeType = 'last';
+    else if(reference[0] !== null && reference[1] !== null)
+      nodeType = 'middle';
+      
+    switch(nodeType){
+      case 'new':
+        //all good here
+      break;
+      case 'first':
+        //all good here
+      break;
+      case 'middle':
+        // previous[next -> ref(update)] <- [previous]current node
+        redisClient.hset(`image:${reference[0]}`, 'next', reference[1], redis.print);
+        // current node[next] -> next[previous -> ref(update)]
+        redisClient.hset(`image:${reference[1]}`, 'previous', reference[0], redis.print);
+      break;
+      case 'last':
+        // previous[next -> ref(update)] <- [previous]current node
+        redisClient.hdel(`image:${reference[0]}`, 'next', redis.print);
+      break;
+    }
+
+    if(lastImageCopy != null && lastImageCopy !== req.file.originalname){
+      //update previous node's previous ref
+      redisClient.hset(`image:${lastImageCopy}`, 'previous', `${req.file.originalname}`, redis.print);
+      //update the current node
+      redisClient.hmset(`image:${req.file.originalname}`, 'next', lastImageCopy, redis.print);
+      // remove previous ref 
+      redisClient.hdel(`image:${req.file.originalname}`, 'previous', redis.print);
+    }
+    
+    if(lastVerifiedImage === req.file.originalname && reference[0] !== null){
+      lastVerifiedImage = reference[0]; //update last verified image
+      redisClient.set('lastVerifiedImage', lastVerifiedImage, redis.print); //persist
+    }
+    
+    lastImage = req.file.originalname; //update last uploaded image
+    redisClient.set('lastImage', lastImage, redis.print); //persist
+
+    res.render('index', { title: 'Upload Successfull', categories: [1,2,3,4,5,6,7,8,9,10], imageName: req.file.originalname, category: req.body.category });
+  });
 });
 
 module.exports = router;
